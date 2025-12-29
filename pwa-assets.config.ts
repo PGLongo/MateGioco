3230039d78d7505fedc226562f8ddf44557b6414
Piergiuseppe Longo <plongo@alteanet.it>
2025-12-29 17:29:57 +0100

    feat(assets): generate complete set of icons and favicons

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,6 +1,23 @@
-import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/config'
+import { defineConfig } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-    preset: minimal2023Preset,
-    images: ['public/icon-1024x1024.svg']
+    images: ['public/icon-1024x1024.svg'],
+    preset: {
+        transparent: {
+            sizes: [64, 192, 512],
+            favicons: [
+                [16, 'favicon-16x16.png'],
+                [32, 'favicon-32x32.png'],
+                [48, 'favicon.ico']
+            ]
+        },
+        maskable: {
+            sizes: [192, 512],
+            padding: 0.1
+        },
+        apple: {
+            sizes: [120, 152, 167, 180],
+            padding: 0
+        }
+    }
 })
