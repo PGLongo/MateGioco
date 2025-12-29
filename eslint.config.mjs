b4fc4f613e6781b4397497bf584be7c7c92cf805
Piergiuseppe Longo <plongo@alteanet.it>
2025-12-29 16:21:26 +0100

    chore: revert missing parser options rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,6 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt(
   // Your custom configs here
   {
-    rules: {
-      '@typescript-eslint/consistent-type-imports': 'error'
-    }
+
   }
 )
